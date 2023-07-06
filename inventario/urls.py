f774bf23f100c2eb88ea472c1b83fbafa5d9dd7b
Julian Estrada <julianestrada@Julians-MacBook-Pro.local>
2023-07-05 21:28:48 -0500

    Add login, product list and product detail view

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import lista_inventario, producto_unitario
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", lista_inventario, name="lista_inventario"),
+    path("detalle_prod/<int:pk>", producto_unitario, name="producto_unitario"),
     ]
--- inventario/views.py
+++ inventario/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Inventario
+from django.contrib.auth.decorators import login_required
 
+@login_required
+def lista_inventario(request):
+    inventarios = Inventario.objects.all()
+    context = {
+        "title": "Listado Inventario",
+        "inventarios": inventarios
+    }
+    return render(request, template_name="inventario/listado_inventario.html", context=context)
 
-def index(request):
-    return render(request, template_name="inventario/index.html")
+@login_required
+def producto_unitario(request, pk):
+    inventario = get_object_or_404(Inventario, pk=pk)
+    context = {
+        "inventario": inventario
+    }
+
+    return render(request, "inventario/producto_detallado.html", context=context)
